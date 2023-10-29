67a5573fa8fd8d9ccc4b0131050caf23ceb0fb3c
parkjian7 <masichyun7@inswave.com>
2023-10-29 23:12:06 +0900

    react-query 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthContextProvider } from './components/context/AuthContext';
 import Header from './components/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 function App() {
-  return <AuthContextProvider>
-    <Navbar />
-    <Header />
-    <Outlet />
-  </AuthContextProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar />
+        <Header />
+        <Outlet />
+      </AuthContextProvider>
+  </QueryClientProvider>
+  )
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import imageData from "../data/carousel.js";
